adb48de3fe5efae511ca14efb5557d2eab6a3495
Bogdan <b.kyryliuk@gmail.com>
2016-08-17 20:05:18 -0700

    Async support for the queries in the SQL Lab. (#974)
    * Refactor the query runner to enable async mode.
    
    * Refactore the sql calling functions into the QueryRunner class.
    
    * Clean up the celery tests.

--- tests/caravel_test_config.py
+++ tests/caravel_test_config.py
@@ -16,13 +16,14 @@ if 'CARAVEL__SQLALCHEMY_DATABASE_URI' in os.environ:
 SQL_CELERY_DB_FILE_PATH = '/tmp/celerydb.sqlite'
 SQL_CELERY_RESULTS_DB_FILE_PATH = '/tmp/celery_results.sqlite'
 SQL_SELECT_AS_CTA = True
+SQL_MAX_ROW = 666
 
 
 class CeleryConfig(object):
     BROKER_URL = 'sqla+sqlite:///' + SQL_CELERY_DB_FILE_PATH
-    CELERY_IMPORTS = ('caravel.tasks', )
+    CELERY_IMPORTS = ('caravel.sql_lab', )
     CELERY_RESULT_BACKEND = 'db+sqlite:///' + SQL_CELERY_RESULTS_DB_FILE_PATH
-    CELERY_ANNOTATIONS = {'tasks.add': {'rate_limit': '10/s'}}
+    CELERY_ANNOTATIONS = {'sql_lab.add': {'rate_limit': '10/s'}}
     CONCURRENCY = 1
 CELERY_CONFIG = CeleryConfig
 
